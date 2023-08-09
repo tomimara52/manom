89223e27a0015a6a43278406f4f2cd71becbda2d
Tomás Maraschio <tomas.maraschio@gmail.com>
2023-08-09 20:02:11 -0300

    nice little comments

--- date.c
+++ date.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+if I start counting from the right of the number:
+from bit 0 to bit 4 is the day
+from 5 to bit 8 is the mont
+the rest of the bits is the year
+*/
 
 date_t create_date(int year, char month, char day) {
     return day | (month << 5) | (year << 9); 
@@ -9,6 +15,8 @@ date_t create_date(int year, char month, char day) {
 
 void print_date(date_t date) {
     printf("%lu/%lu/%lu", date >> 9, (date >> 5) & 15, date & 31);
+    // & 15 is representing & 0b1111 (keep the last 4 bits)
+    // & 31 is representing & 0b11111 (keep the last 5 bits)
 }
 
 char* date_to_str(date_t date) {
