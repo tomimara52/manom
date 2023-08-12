4bf31e4ec86ae3b87b729ed502559a28939b17fd
Tomás Maraschio <tomas.maraschio@gmail.com>
2023-08-12 17:29:37 -0300

    make use of utils

--- entry.c
+++ entry.c
@@ -1,9 +1,10 @@
-#include "entry.h"
-#include "date.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
+
+#include "entry.h"
+#include "date.h"
+#include "utils.h"
 
 struct entry_s{
     char* content;
@@ -79,33 +80,9 @@ char* get_entry_content(entry_t entry) {
 }
 
 entry_t create_entry_from_stdin() {
-    char text[256];
     char* content = NULL;
-    unsigned int acc_size = 0;
 
-    while (1) {
-        char* check = fgets(text, 256, stdin);
-
-        // if read EOF
-        if (check == NULL)
-            break;
-
-        unsigned int read_length = strlen(text);
-
-        // realloc to the size it has + the amount i read + the null character
-        content = realloc(content, acc_size + read_length + 1);
-        // write in content + acc_size to not overwrite previous
-        sprintf(content + acc_size, "%s", text);
-        // only add read_length and not + 1 for null character because I only want a null
-        // character at the end
-        acc_size += read_length;
-
-     
-        // if EOF sent in middle of input
-        if (feof(stdin)) {
-            break;
-        }
-    }
+    unsigned int acc_size = undef_length_str_stdin(&content);
 
     time_t res = time(NULL);
     struct tm* lcl_tm = localtime(&res);
@@ -114,10 +91,6 @@ entry_t create_entry_from_stdin() {
                                  create_date(lcl_tm->tm_year + 1900, lcl_tm->tm_mon + 1, lcl_tm->tm_mday));
 
     free(content);
-
-    // make stdin not EOF
-    clearerr(stdin);
-
 
     return entry;
 }
